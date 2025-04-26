11974d1ba3df9fe1b53360c5b59290872bfc8988
TiagoViniciusDev <tiagoviniciusdev@gmail.com>
2025-04-26 20:40:11 -0300

    Mudando btn do curriculo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -18,7 +18,7 @@ function Home() {
             <p>Programador empenhado a criar aplicações otimizadas e responsivas</p>
             <div data-aos="fade-left" data-aos-duration="1000" data-aos-anchor-placement="top">
               <a href={Data.EmailLink} rel="noreferrer" target="_blank">Entre em contato</a>
-              <a href={`./cv/${Data.curriculo}`} download={Data.curriculo}>Currículo</a>
+              <a href={`./cv/${Data.curriculo}`} target="_blank" rel="noopener noreferrer">Currículo</a>
             </div>
         </section>
     </div>
