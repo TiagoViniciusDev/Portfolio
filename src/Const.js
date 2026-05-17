218021d86078f21999a1d44397fbc408ac8cd2be
TiagoViniciusDev <tiagoviniciusdev@gmail.com>
2026-05-17 07:28:07 -0300

    Atualização Link do linkedin

--- src/Const.js
+++ src/Const.js
@@ -2,8 +2,7 @@
 
 const Data = {
     //Informações Pessoais
-    WhatsappLink: 'https://wa.me/5586995652410?text=Ol%C3%A1%2C+vi+seu+portf%C3%B3lio+',
-    LinkedinLink: 'https://www.linkedin.com/in/tiagoviniciusdev/',
+    LinkedinLink: 'https://www.linkedin.com/in/tiagoviniciusdesenvolvedor/',
     GitHubLink: 'https://github.com/TiagoViniciusDev',
     EmailLink: 'mailto:tiagoviniciusdev@gmail.com',
     curriculo: 'Tiago_Vinicius.pdf'
